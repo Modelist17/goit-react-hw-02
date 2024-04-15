3e196918240a5440ad6d5c1bd3de94fca03117d1
Modelist17 <hunteruboat@gmail.com>
2024-04-15 11:13:36 +0200

    descr+feedback

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import css from './Description.module.css'
+import React from "react";
+import css from "./Description.module.css";
 
-const Description = ({title,text}) => {
+const Description = () => {
+  const title = "Sip Happens Café";
+  const text = "Please leave your feedback about our service by selecting one of the options below.";
+
   return (
     <>
-    <h1 className={css.title}>{title}</h1>
-    <p className={css.text}>{text}</p>
+      <h1 className={css.title}>{title}</h1>
+      <p className={css.text}>{text}</p>
     </>
-  )
-}
+  );
+};
 
-export default Description
+export default Description;
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import css from './Feedback.module.css';
 
-const Feedback = ({ feedback, totalFeedback, positiveFeedback }) => {
+const Feedback = ({ feedback }) => {
   const feedbackStatus = Object.keys(feedback);
+  const totalFeedback = Object.values(feedback).reduce((acc, curr) => acc + curr, 0);
+  const positiveFeedback = totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+
   return (
     <div className={css.field}>
-      {feedbackStatus.map(item => {
-        const feedbackValues = feedback;
-        return (
-          <p className={css.title} key={item}>
-            {item}: {feedbackValues[item]}
-          </p>
-        );
-      })}
+      {feedbackStatus.map(item => (
+        <p className={css.title} key={item}>
+          {item}: {feedback[item]}
+        </p>
+      ))}
       {totalFeedback > 0 && <p className={css.title}>Total: {totalFeedback}</p>}
       {totalFeedback > 0 && <p className={css.title}>Positive: {positiveFeedback}%</p>}
-
     </div>
   );
 };
